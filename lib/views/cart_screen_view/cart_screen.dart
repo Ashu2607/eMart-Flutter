df874cbbf4471ac4e817a7bf371eb8f78a74a4aa
Usama <epiicuree@gmail.com>
2023-11-16 19:00:59 +0500

    Add code to Cart screen UI

--- lib/views/cart_screen_view/cart_screen.dart
+++ lib/views/cart_screen_view/cart_screen.dart
@@ -1,10 +1,17 @@
-import 'package:flutter/material.dart';
+import 'package:emart/consts/consts.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.cyan);
+    return Container(
+      color: Colors.white,
+      child: "Cart is empty"
+          .text
+          .fontFamily(semibold)
+          .color(darkFontGrey)
+          .makeCentered(),
+    );
   }
 }

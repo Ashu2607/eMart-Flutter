2f8d202669391e89718f34c80d08e09b17f748bf
Usama <epiicuree@gmail.com>
2023-11-19 15:16:52 +0500

    Add Remove from Wishlist Screen

--- lib/views/wishlist_screen_view/wishlist_screen.dart
+++ lib/views/wishlist_screen_view/wishlist_screen.dart
@@ -21,9 +21,55 @@ class WishlistScreen extends StatelessWidget {
           if (!snapshot.hasData)
             return loadingIndicator();
           else if (snapshot.data!.docs.isEmpty)
-            return "No items yet!".text.color(darkFontGrey).makeCentered();
-          else
-            return Container();
+            return "Wishlist is Empty".text.color(darkFontGrey).makeCentered();
+          else {
+            var data = snapshot.data!.docs;
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: data.length,
+                    shrinkWrap: true,
+                    itemBuilder: (BuildContext context, int index) => ListTile(
+                      leading: Image.network(
+                        "${data[index]['p_images'][0]}",
+                        width: 80,
+                        fit: BoxFit.cover,
+                      ),
+                      title: "${data[index]['p_name']}"
+                          .text
+                          .fontFamily(semibold)
+                          .size(16)
+                          .make(),
+                      subtitle: "${data[index]['p_price']}"
+                          .numCurrency
+                          .text
+                          .fontFamily(semibold)
+                          .size(16)
+                          .color(redColor)
+                          .make(),
+                      trailing: IconButton(
+                        onPressed: () => firestore
+                            .collection(productsCollection)
+                            .doc(data[index].id)
+                            .set(
+                          {
+                            'p_wishlist':
+                                FieldValue.arrayRemove([currentUser!.uid])
+                          },
+                          SetOptions(merge: true),
+                        ),
+                        icon: const Icon(
+                          Icons.favorite_outlined,
+                          color: redColor,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }
         },
       ),
     );

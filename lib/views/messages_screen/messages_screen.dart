f156908937a3397de80042f3919622813fd7d2fc
Usama <epiicuree@gmail.com>
2023-11-19 23:29:35 +0500

    Add Message box separate screen

--- lib/views/messages_screen/messages_screen.dart
+++ lib/views/messages_screen/messages_screen.dart
@@ -3,6 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emart/common_widgets/loading_indicator.dart';
 import 'package:emart/consts/consts.dart';
 import 'package:emart/services/firestore_service.dart';
+import 'package:emart/views/chat_screen_view/chat_screen.dart';
+import 'package:get/get.dart';
 
 class MessagesScreen extends StatelessWidget {
   const MessagesScreen({super.key});
@@ -13,7 +15,7 @@ class MessagesScreen extends StatelessWidget {
       backgroundColor: whiteColor,
       appBar: AppBar(
         title:
-            "My Wishlist".text.color(darkFontGrey).fontFamily(semibold).make(),
+            "My Messages".text.color(darkFontGrey).fontFamily(semibold).make(),
       ),
       body: StreamBuilder(
         stream: FirestoreService.getAllMessages(),
@@ -22,8 +24,41 @@ class MessagesScreen extends StatelessWidget {
             return loadingIndicator();
           else if (snapshot.data!.docs.isEmpty)
             return "No messages yet!".text.color(darkFontGrey).makeCentered();
-          else
-            return Container();
+          else {
+            var data = snapshot.data!.docs;
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: data.length,
+                      itemBuilder: (BuildContext context, int index) => Card(
+                        child: ListTile(
+                          onTap: () => Get.to(() => const ChatScreen(),
+                              arguments: [
+                                data[index]['friend_name'],
+                                data[index]['toId']
+                              ]),
+                          leading: const CircleAvatar(
+                            backgroundColor: redColor,
+                            child: Icon(Icons.person),
+                          ),
+                          title: "${data[index]['friend_name']}"
+                              .text
+                              .fontFamily(semibold)
+                              .color(darkFontGrey)
+                              .make(),
+                          subtitle:
+                              "${data[index]['last_message']}".text.make(),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
         },
       ),
     );

1ad2e99c9126936aa13dda24b8e47b53a6b6f584
Usama <epiicuree@gmail.com>
2023-11-20 17:39:42 +0500

    Add device preview package

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,14 @@ import 'package:emart/firebase_options.dart';
 import 'package:emart/views/splash_screen_view/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MainApp());
+  runApp(DevicePreview(builder: (context) => const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -19,6 +20,9 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: appname,
       theme: ThemeData(
         scaffoldBackgroundColor: Colors.transparent,
@@ -33,5 +37,3 @@ class MainApp extends StatelessWidget {
     );
   }
 }
-
-

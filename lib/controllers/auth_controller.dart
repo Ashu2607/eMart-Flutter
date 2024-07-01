eb3f7671b70c292036548a736340eb6c19ed1f85
Ashu2607 <ashutosh.snh@iitg.ac.in>
2024-07-01 16:58:38 +0530

    Update auth_controller.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -2,42 +2,52 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emart/consts/consts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 
 class AuthController extends GetxController {
   var isLoading = false.obs;
 
   // login method
-
-  Future<UserCredential?> loginMethod({email, password, context}) async {
+  Future<UserCredential?> loginMethod({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
     UserCredential? userCredential;
     try {
       userCredential = await auth.signInWithEmailAndPassword(
           email: email, password: password);
     } on FirebaseAuthException catch (e) {
-      VxToast.show(context, msg: e.toString());
+      VxToast.show(context, msg: e.message ?? "An error occurred");
     }
     return userCredential;
   }
 
   // signup method
-  Future<UserCredential?> signupMethod({email, password, context}) async {
+  Future<UserCredential?> signupMethod({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
     UserCredential? userCredential;
     try {
       userCredential = await auth.createUserWithEmailAndPassword(
           email: email, password: password);
     } on FirebaseAuthException catch (e) {
-      VxToast.show(context, msg: e.toString());
+      VxToast.show(context, msg: e.message ?? "An error occurred");
     }
     return userCredential;
   }
 
   // store user data
-  storeUserdata({name, password, email}) async {
+  Future<void> storeUserdata({
+    required String name,
+    required String email,
+  }) async {
     DocumentReference store =
         firestore.collection(usersCollection).doc(currentUser!.uid);
     await store.set({
       'name': name,
-      'password': password,
       'email': email,
       'imageUrl': '',
       'id': currentUser!.uid,
@@ -48,7 +58,7 @@ class AuthController extends GetxController {
   }
 
   // signout
-  signoutMethod({context}) async {
+  Future<void> signoutMethod({required BuildContext context}) async {
     try {
       await auth.signOut();
     } catch (e) {
